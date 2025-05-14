e555f7da2ab9824e72f692e1883d6e397de607b8
GatiMhersi <56447063+GatiMhersi@users.noreply.github.com>
2025-05-14 19:52:26 -0300

    Add url in images external config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['res.cloudinary.com'], // agregá aquí cualquier dominio adicional también
+  },
 };
 
 export default nextConfig;
